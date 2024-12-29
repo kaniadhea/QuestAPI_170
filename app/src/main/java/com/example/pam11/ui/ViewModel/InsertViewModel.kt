7ba2e720605ea119b6366cd0e72075b6c65f6a7b
kaniadhea <kaniadhea7@gmail.com>
2024-12-29 17:27:50 +0700

    membuat fungsi di InsertViewModel.kt

---
+++ app/src/main/java/com/example/pam11/ui/ViewModel/InsertViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.pam11.ui.ViewModel
+
+import com.example.pam11.model.Mahasiswa
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+
+    val nim: String="",
+    val nama: String="",
+    val alamat: String="",
+    val jenisKelamin: String="",
+    val kelas: String="",
+    val angkatan: String=""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
