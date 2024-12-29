395662f76f28c5ed3d000a2393ce869cc6e4d7d7
kaniadhea <kaniadhea7@gmail.com>
2024-12-29 16:55:42 +0700

    kontakrepo akan dibuat hanya saat dibutuhkan

--- app/src/main/java/com/example/pam11/container/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pam11/container/MahasiswaContainer.kt
@@ -1,7 +1,12 @@
 package com.example.pam11.container
 
 import com.example.pam11.Repository.MahasiswaRepository
+import com.example.pam11.Repository.NetworkKontakRepository
+import com.example.pam11.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 interface AppContainer{
     val kontakRepository:MahasiswaRepository
@@ -11,5 +16,15 @@ class MahasiswaContainer: AppContainer {
 
     private val baseUrl= ""
     private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
 
+    private val mahasiswaService: MahasiswaService by  lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkKontakRepository(mahasiswaService)
+    }
 }
