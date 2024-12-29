4c2779fdbc6cde64e910fdc9a7b4e7aaa69e6bac
kaniadhea <kaniadhea7@gmail.com>
2024-12-29 20:09:47 +0700

    fun entrybody utk bagian formulir

--- app/src/main/java/com/example/pam11/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pam11/ui/view/InsertView.kt
@@ -4,14 +4,46 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.pam11.ui.ViewModel.InsertUiEvent
+
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiEvent,
+    onSiswaValueChange:(InsertUiEvent)->Unit,
+    onSaveClick:()->Unit,
+    modifier: Modifier=Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+
+}
+
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
