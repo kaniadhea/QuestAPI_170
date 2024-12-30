50412b089fbd67ebe5dd509919fe7d93b2e2e7c7
kaniadhea <kaniadhea7@gmail.com>
2024-12-30 09:46:58 +0700

    membuat fungsi mhs

---
+++ app/src/main/java/com/example/pam11/ui/ViewModel/DetailViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.pam11.ui.ViewModel
+
+import com.example.pam11.model.Mahasiswa
+
+fun Mahasiswa.toDetailUiEvent(): InsertUiEvent{
+    return InsertUiEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
