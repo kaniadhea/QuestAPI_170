f743ecb106babb28d17654241d59b1081e9dd410
kaniadhea <kaniadhea7@gmail.com>
2024-12-30 10:43:13 +0700

    membuat body di view

--- app/src/main/java/com/example/pam11/ui/view/DetailView.kt
+++ app/src/main/java/com/example/pam11/ui/view/DetailView.kt
@@ -1,17 +1,25 @@
 package com.example.pam11.ui.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.KeyEventDispatcher.Component
 import com.example.pam11.model.Mahasiswa
+import com.example.pam11.ui.ViewModel.DetailUiState
 import com.example.pam11.ui.navigasi.DestinasiNavigasi
 
 object DestinasiDetail : DestinasiNavigasi{
@@ -21,29 +29,86 @@ object DestinasiDetail : DestinasiNavigasi{
     override val titleRes = "Detail Mhs"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun
+
+
+@Composable
+fun BodyDetail(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState,
+    retryAction: () -> Unit = {}
+) {
+    when (detailUiState) {
+        is DetailUiState.Loading -> {
+            OnLoading(modifier = modifier.fillMaxSize())
+        }
+        is DetailUiState.Success -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetail(mahasiswa = detailUiState.mahasiswa)
+            }
+        }
+        is DetailUiState.Error -> {
+            OnError(
+                retryAction = retryAction,
+                modifier = modifier.fillMaxSize()
+            )
+        }
+        else -> {
+            Text("Unexpected state encountered")
+        }
+    }
+
+}
+
 @Composable
 fun ItemDetail(
     mahasiswa : Mahasiswa
 ){
-    Card
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            ComponentDetail(judul = "NIM", isinya = mahasiswa.nim)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetail(judul = "Nama", isinya = mahasiswa.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetail(judul = "Alamat", isinya = mahasiswa.alamat)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetail(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetail(judul = "Kelas", isinya = mahasiswa.kelas)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetail(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        }
+    }
 }
 
 
 
 @Composable
-fun ComponetDetail(
+fun ComponentDetail(
     judul: String,
     isinya: String
-){
-    Column (
+) {
+    Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.Start
-    ){
+    ) {
         Text(
             text = "$judul :",
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
-            color = Color.DarkGray
+            color = Color.Gray
         )
         Text(
             text = isinya,
