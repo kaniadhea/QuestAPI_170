070cbbf039ac5b46f8db4681245650083b5058c7
kaniadhea <kaniadhea7@gmail.com>
2024-12-30 11:43:49 +0700

    membuat fungsi toapbar

---
+++ app/src/main/java/com/example/pam11/customwidget.kt
@@ -0,0 +1,23 @@
+package com.example.pam11
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {},
+){
+    CenterAlignedTopAppBar(
+
+    )
+
+
+}
