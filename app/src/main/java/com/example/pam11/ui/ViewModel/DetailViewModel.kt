bc042627fc43962cdd65d718f9b9587cd9692050
kaniadhea <kaniadhea7@gmail.com>
2024-12-30 09:50:00 +0700

    membuat class detail pada viewmodel

--- app/src/main/java/com/example/pam11/ui/ViewModel/DetailViewModel.kt
+++ app/src/main/java/com/example/pam11/ui/ViewModel/DetailViewModel.kt
@@ -1,6 +1,22 @@
 package com.example.pam11.ui.ViewModel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.pam11.Repository.MahasiswaRepository
 import com.example.pam11.model.Mahasiswa
+
+sealed class DetailUiState{
+    data class Success(val mahasiswa: Mahasiswa): DetailUiState()
+    object Error : DetailUiState()
+    object Loading : DetailUiState()
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mhs : MahasiswaRepository
+) : ViewModel(){
+
+}
 
 fun Mahasiswa.toDetailUiEvent(): InsertUiEvent{
     return InsertUiEvent(
