853e013b60ea9ce1a529eff9756a9721337251a8
kaniadhea <kaniadhea7@gmail.com>
2024-12-30 12:50:02 +0700

    membuat CostumeTopAppBar

--- app/src/main/java/com/example/pam11/customwidget.kt
+++ app/src/main/java/com/example/pam11/customwidget.kt
@@ -1,6 +1,14 @@
 package com.example.pam11
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,10 +22,24 @@ fun CostumeTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {},
     onRefresh: () -> Unit = {},
-){
+) {
     CenterAlignedTopAppBar(
-
+        title = { Text(title) },
+        actions = {
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = "",
+                modifier = Modifier.clickable {
+                    onRefresh()
+                })
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior, navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                }
+            }
+        }
     )
-
-
 }
