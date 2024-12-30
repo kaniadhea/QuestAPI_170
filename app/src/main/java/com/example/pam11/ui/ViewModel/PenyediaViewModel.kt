c9d00cf4d35ca2b3ee554484251d8ae5a3d32b11
kaniadhea <kaniadhea7@gmail.com>
2024-12-30 12:54:24 +0700

    menambahkan initia detail dan update

--- app/src/main/java/com/example/pam11/ui/ViewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam11/ui/ViewModel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.pam11.ui.ViewModel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,6 +11,9 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiKontak().container.mahasiswaRepository) }
         initializer { InsertViewModel(aplikasiKontak().container.mahasiswaRepository) }
+        initializer { DetailViewModel(createSavedStateHandle(),aplikasiKontak().container.mahasiswaRepository) }
+        initializer { UpdateViewModel(createSavedStateHandle(),aplikasiKontak().container.mahasiswaRepository) }
+
     }
 }
 
